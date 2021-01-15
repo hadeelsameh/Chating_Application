4889f2559edf7e02ef73af2f32d33df63d1a7b88
hadeelsameh <43917398+hadeelsameh@users.noreply.github.com>
2021-01-15 10:47:33 +0200

    Handling Idle Clients

--- Server_ii.py
+++ Server_ii.py
@@ -12,57 +12,57 @@ DISCONNECT_MESSAGE = "!DISCONNECT" # if sent by the client it will be disconnect
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # starting the server with TCP
 server.bind(ADDR)
 
-Clients = []
-Clients_Names = []
+Clients = [] #list to save iddress of conected clients
+Clients_Names = [] #list to save user name of conected clients
 
 
 # Sending Messages To All Connected Clients
 def broadcast(message , sending_client):
     for client in Clients:
         if client == sending_client:
+            #incoding msg in jason format
             data = {"message": message , "identitiy": "Me" }
             print(f'sending_client = {sending_client}')
         else: 
-            index =Clients.index(sending_client)
+            index =Clients.index(sending_client) #getting index of the conn in array
             data = {"message": message , "identitiy": Clients_Names[index] }
-        data_string = json.dumps(data)
+        data_string = json.dumps(data) #serializing meassage in to string
         print("data" , data_string)
-        client.send(data_string.encode(FORMAT))
-        # client.send(message)
+        client.send(data_string.encode(FORMAT)) #sending message to clients
+        
 
 
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-
+    index = Clients.index(conn)
+    print(f"[NEW CONNECTION]  {addr} ( {Clients_Names[index]} ) had connected.")
     connected = True
-    while connected:
-        msg_lenght = conn.recv(HEADER).decode(FORMAT)
-        if msg_lenght:
-            # try:
-            msg_lenght = int(msg_lenght)
-            msg = conn.recv(msg_lenght).decode(FORMAT)
-            # msg = conn.recv(msg_lenght)
-            broadcast(msg, conn)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-                index = Clients.index(conn)
-                Clients.remove(conn)
-                Clients_Names.remove(Clients_Names[index])
-                print(f"[{addr}] left!")
-            print(f"[{addr}] {msg}")
-
-                # conn.send("Msg received".encode(FORMAT))
-            # except:
-            #     # Removing And Closing Clients
-            #     # index = Clients.index(conn)
-            #     Clients.remove(conn)
-            #     conn.close()
-            #     print(f"[{addr}] left!")
-            #     # nickname = nicknames[index]
-            #     # broadcast('{} left!'.format(nickname).encode('ascii'))
-            #     # nicknames.remove(nickname)
-            #     break
-    conn.close()
+    try:
+        while connected:
+            conn.settimeout(100) #setting timeout after 100 second (it will raise time out Exception if client was idle)
+            msg_lenght = conn.recv(HEADER).decode(FORMAT) 
+            conn.settimeout(None) # cancleing timeout timer as the client sent message header
+            if msg_lenght:
+                # try:
+                msg_lenght = int(msg_lenght)
+                msg = conn.recv(msg_lenght).decode(FORMAT)
+                broadcast(msg, conn)
+                if msg == DISCONNECT_MESSAGE:
+                    connected = False
+                    #removing client data from clients and names lists
+                    index = Clients.index(conn) #getting index of the conn in array
+                    Clients.remove(conn)
+                    Clients_Names.remove(Clients_Names[index])
+                    print(f"[{addr}] left!")
+                print(f"[{addr}] {msg}")
+    except Exception: #occurs when client is idle
+        connected = False
+        index = Clients.index(conn)
+        #removing client data from clients and names lists
+        Clients.remove(conn) 
+        Clients_Names.remove(Clients_Names[index])
+        print(f"[{addr}] was disconnected (Idle client)")
+        
+    conn.close() #closing connection 
 
 
 
@@ -71,12 +71,13 @@ def start():
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         conn , adrr = server.accept()
-        Clients.append(conn)
-        Clients_Names.append(conn.recv(HEADER).decode(FORMAT))
-        print(Clients_Names)
-        thread = threading.Thread(target=handle_client, args=(conn, adrr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        Clients.append(conn) # appending new conn in array
+        Clients_Names.append(conn.recv(HEADER).decode(FORMAT)) # appending new conn user name in array
+        #print(Clients_Names)
+        thread = threading.Thread(target=handle_client, args=(conn, adrr)) #initializing thread for every client
+        thread.start() #starting thread
+        print(f"[ACTIVE CONNECTIONS]  : {threading.activeCount() - 1}") #number of threads = number of clients
+
 
 print("[STARTING] server is starting...")
 start()
